f0880c7558d6fa000caa4416f015e81cfd8e06c1
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-09-12 14:48:58 +0530

    resize and dscvr wallet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import type { Metadata } from "next";
 import { Inter, Syne } from "next/font/google";
@@ -13,20 +14,20 @@ import { registerCanvasWallet } from "@dscvr-one/canvas-wallet-adapter";
 const inter = Inter({ subsets: ["latin"] });
 const syne = Syne({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "DAO",
-  description: "Create DAO Voting",
-  openGraph: {
-    title: "DAO",
-    description: "Create DAO Voting - Powered by Solana",
-    type: "website",
-    url: "https://dao-frontend-beta.vercel.app/",
-    images: "https://news.miami.edu/_assets/images-stories/2023/02/dao-web3-hero-940x529.jpg"
-  },
-  other: {
-    'dscvr:canvas:version': "vNext",
-  }
-};
+// export const metadata: Metadata = {
+//   title: "DAO",
+//   description: "Create DAO Voting",
+//   openGraph: {
+//     title: "DAO",
+//     description: "Create DAO Voting - Powered by Solana",
+//     type: "website",
+//     url: "https://dao-frontend-beta.vercel.app/",
+//     images: "https://news.miami.edu/_assets/images-stories/2023/02/dao-web3-hero-940x529.jpg"
+//   },
+//   other: {
+//     'dscvr:canvas:version': "vNext",
+//   }
+// };
 
 
 export default function RootLayout({
